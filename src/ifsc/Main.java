1fbc2e6980f1fd307a6e9f50ae91f794b5d4e668
Jackson <lordjackson@gmail.com>
2021-08-02 11:54:08 -0300

    Add Codigo Slides 08 - Exercicios lista 1 e lista 2 - Q01 e Q03

---
+++ src/ifsc/Main.java
@@ -0,0 +1,8 @@
+package ifsc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Teste");
+    }
+}
---
+++ src/ifsc/exercicios/lista01/Exercicio05.java
@@ -0,0 +1,26 @@
+package ifsc.exercicios.lista01;
+
+import java.util.Scanner;
+
+/*
+Q05 - Elaborar um algoritmo para calcular e apresentar o volume de uma lata de óleo,
+    utilizando a fórmula, utilize uma variável PI como constante:
+	VOLUME = 3.14159 * (R*R) * ALTURA
+ */
+public class Exercicio05 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double volume, raio, altura;
+
+        System.out.println("Informe o raio");
+        raio = teclado.nextDouble();
+
+        System.out.println("Informe a altura");
+        altura = teclado.nextDouble();
+
+        volume = Math.PI * Math.pow(raio, 2) * altura;
+
+        System.out.printf("O volume é: %.2f", volume);
+
+    }
+}
